798d00b9d5f48a3b94d19a246798001977c8d262
yuminhao107 <yuminhao107@gmail.com>
2019-10-15 15:10:20 -0500

    add L1071

---
+++ src/leetcode/L1071.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class L1071 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String gcdOfStrings(String str1, String str2) {
+        
+		
+		return "";
+    }
+
+}
